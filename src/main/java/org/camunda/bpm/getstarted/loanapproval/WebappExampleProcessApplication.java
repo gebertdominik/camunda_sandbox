8c1c3c14c4d2139e53c1e2941e883870da987c82
dgebert <dominik.gebert@symmetry.pl>
2018-04-26 14:18:07 +0200

    Add Spring Boot support

---
+++ src/main/java/org/camunda/bpm/getstarted/loanapproval/WebappExampleProcessApplication.java
@@ -0,0 +1,13 @@
+package org.camunda.bpm.getstarted.loanapproval;
+
+import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@EnableProcessApplication
+@SpringBootApplication
+public class WebappExampleProcessApplication {
+    public static void main(String... args) {
+        SpringApplication.run(WebappExampleProcessApplication.class, args);
+    }
+}
